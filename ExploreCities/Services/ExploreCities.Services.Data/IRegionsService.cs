2154e2e92e012439b8737c0051328d08d02e0406
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-07-23 21:05:42 +0300

    Make Service to Automatic Region Create

---
+++ ExploreCities/Services/ExploreCities.Services.Data/IRegionsService.cs
@@ -0,0 +1,11 @@
+﻿namespace ExploreCities.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IRegionsService
+    {
+        Task CreateAsync(string name, string cityId);
+
+        string GetRegionId(string name);
+    }
+}
