b1a2c4f993e930434b76d87a3369d0b85b047c7d
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-07-23 20:59:20 +0300

    Add RegionView Create and SelectList (Cities) View Models

---
+++ ExploreCities/Web/ExploreCities.Web.ViewModels/RegionViews/CitySelectListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExploreCities.Web.ViewModels.RegionViews
+{
+    public class CitySelectListViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ExploreCities/Web/ExploreCities.Web.ViewModels/RegionViews/CreateRegionViewInputModel.cs
@@ -0,0 +1,57 @@
+﻿namespace ExploreCities.Web.ViewModels.RegionViews
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateRegionViewInputModel
+    {
+        [Required]
+        [Display(Name ="City name")]
+        public string CityId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"^[A-Z][a-z]+$")]
+        [Display(Name ="Region name")]
+        public string RegionName { get; set; }
+
+        [Range(1930, 2200)]
+        [Display(Name ="Arrival year")]
+        public int ArrivalYear { get; set; }
+
+        [Display(Name ="Departure year")]
+        public int? DepartureYear { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Comment { get; set; }
+
+        [Required]
+        [Url]
+        [Display(Name ="Image URL")]
+        public string PictureUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Parking Spaces")]
+        public string ParkingSpacesExistence { get; set; }
+
+        [Required]
+        [Display(Name = "Playgrounds")]
+        public string ChildrenPlaygroundsExistence { get; set; }
+
+        [Required]
+        [Display(Name = "Air pollution")]
+        public string AirPollutionRating { get; set; }
+
+        [Required]
+        [Display(Name = "Noise")]
+        public string NoiseRating { get; set; }
+
+        [Required]
+        [Display(Name = "Public transport")]
+        public string PublicTransportRating { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> Cities { get; set; }
+    }
+}
