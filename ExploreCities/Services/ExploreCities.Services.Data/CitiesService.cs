09a8199c05fc708939dcc590b07970ed2fe7044f
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-07-23 21:04:12 +0300

    Create Service to Get SelectList from Cities

---
+++ ExploreCities/Services/ExploreCities.Services.Data/CitiesService.cs
@@ -0,0 +1,31 @@
+﻿namespace ExploreCities.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ExploreCities.Data.Common.Repositories;
+    using ExploreCities.Data.Models.Location;
+
+    public class CitiesService : ICitiesService
+    {
+        private readonly IDeletableEntityRepository<City> citiesRepository;
+
+        public CitiesService(
+            IDeletableEntityRepository<City> citiesRepository)
+        {
+            this.citiesRepository = citiesRepository;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> GetAllAsKeyValuePairs()
+        {
+            return this.citiesRepository.AllAsNoTracking()
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                })
+                .OrderBy(x => x.Name)
+                .ToList().Select(x => new KeyValuePair<string, string>(x.Id.ToString(), x.Name));
+        }
+    }
+}
---
+++ ExploreCities/Services/ExploreCities.Services.Data/ICitiesService.cs
@@ -0,0 +1,9 @@
+﻿namespace ExploreCities.Services.Data
+{
+    using System.Collections.Generic;
+
+    public interface ICitiesService
+    {
+        IEnumerable<KeyValuePair<string, string>> GetAllAsKeyValuePairs();
+    }
+}
