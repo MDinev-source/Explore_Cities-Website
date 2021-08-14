403fa36ce237c12a0f27bdc902021f563deef58c
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-08-14 22:14:35 +0300

    Building a Basic Vision of the Home Page

--- ExploreCities/ExploreCities.Common/GlobalConstants.cs
+++ ExploreCities/ExploreCities.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "ExploreCities";
+        public const string SystemName = "Explore Cities";
 
         public const string AdministratorRoleName = "Administrator";
     }
