30ec2db3b113e41d9684db4d4933a1cc3ec09047
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-08-18 21:41:19 +0300

    Add Pictures Views and Controller To Administration Area

---
+++ ExploreCities/Web/ExploreCities.Web/Areas/Administration/Controllers/PicturesController.cs
@@ -0,0 +1,58 @@
+﻿namespace ExploreCities.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using ExploreCities.Services.Data;
+    using ExploreCities.Web.ViewModels.Pictures;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PicturesController : AdministrationController
+    {
+        private readonly IPicturesService picturesService;
+
+        public PicturesController(
+            IPicturesService picturesService)
+        {
+            this.picturesService = picturesService;
+        }
+
+        public async Task<IActionResult> All(AllObjectPicturesViewModel allObjectPicturesViewModel)
+        {
+            var objectPictures = await this.picturesService.GetAllDbPicturesAsync();
+
+            allObjectPicturesViewModel.Pictures = objectPictures;
+
+            return this.View(allObjectPicturesViewModel);
+        }
+
+        public async Task<IActionResult> Carousel(AllObjectPicturesViewModel allObjectPicturesViewModel, string pictureId)
+        {
+            var objectPictures = await this.picturesService.GetAllDbPicturesAsync();
+
+            allObjectPicturesViewModel.Pictures = objectPictures;
+
+            allObjectPicturesViewModel.CarouselIndex = this.picturesService.GetIndexForAllPics(pictureId);
+
+            return this.View(allObjectPicturesViewModel);
+        }
+
+        public async Task<IActionResult> Delete(string id, string objectId)
+        {
+            var pictureToDelete = await this.picturesService.GetDeleteViewModelByIdAsync(id, objectId);
+
+            return this.View(pictureToDelete);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(PictureDeleteViewModel pictureDeleteViewModel)
+        {
+            var id = pictureDeleteViewModel.Id;
+
+            var objectId = this.picturesService.GetPicture(id).DistrictObjectId;
+
+            await this.picturesService.DeleteByIdAsync(id);
+
+            return this.RedirectToAction(nameof(this.All), new { objectId });
+        }
+    }
+}
