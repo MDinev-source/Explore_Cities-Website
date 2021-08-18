65f30c63c5fc853953e9178a7f1b6ae2b86b4ac8
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-08-18 19:35:54 +0300

    Create Admin Info Constants

--- ExploreCities/ExploreCities.Common/GlobalConstants.cs
+++ ExploreCities/ExploreCities.Common/GlobalConstants.cs
@@ -5,5 +5,11 @@
         public const string SystemName = "Explore Cities";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string AdministratorUsername = "admin@explorecities.com";
+
+        public const string AdministratorEmail = "admin@explorecities.com";
+
+        public const string AdministratorPassword = "AdminPass123";
     }
 }
